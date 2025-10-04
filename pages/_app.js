6c6b04913bdec561c23b1de8c0d1a2af0b5b6b69
Theodoros Panagopoulos <theodoros.panagopoulos@dialecticanet.com>
2025-10-04 12:52:31 +0300

    added ms clarity

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,32 @@
 import "./style.css";
+import Script from "next/script";
 
-export default function MyApp({
-  Component: Component,
-  pageProps: pageProps
-}) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  const clarityProjectId = process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID;
+  const claritySnippet = clarityProjectId
+    ? `
+        (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", ${JSON.stringify(clarityProjectId)});
+      `
+        .replace(/\s+/g, " ")
+        .trim()
+    : null;
+
+  return (
+    <>
+      {claritySnippet ? (
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: claritySnippet,
+          }}
+        />
+      ) : null}
+      <Component {...pageProps} />
+    </>
+  );
 }
